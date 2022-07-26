bb89baef2ec166cbc3562ab91f93e2673b2b69fe
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-07-26 16:44:28 +0300

    Creatin Styled Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
 
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+`;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div>
-      Merhaba
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>Video Card</Wrapper>
+      </Main>
+    </Container>
   );
 }
 
