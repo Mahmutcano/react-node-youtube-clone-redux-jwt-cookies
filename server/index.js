578b13ef476436fa1b4615ccba95c7b8f260c0ae
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-07-27 16:17:30 +0300

    Server MVC && Mongo && Model

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Mongoya bağlandı");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
 
 // PORT
-app.listen(8000, () => {
+app.listen(8800, () => {
+  connect()
   console.log("Bağlandı");
 });
