a852f175433ca6a978b6c1afb1299fe431aee618
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-07-27 12:00:07 +0300

    React Router Dom 6 && Video Card Components

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import { darkTheme, lightTheme } from "./utils/Theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Video from "./pages/Video";
 
 const Container = styled.div`
   display: flex;
@@ -20,64 +23,24 @@ function App() {
   const [darkMode, setDarkMode] = useState(true);
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    <Container>
-      <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
-      <Main>
-        <Navbar />
-        <Wrapper>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-          <h1>Test</h1>
-        </Wrapper>
-      </Main>
-    </Container>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home/>} />
+                  <Route path="video">
+                    <Route path=":id" element={<Video/>} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
     </ThemeProvider>
   );
 }
