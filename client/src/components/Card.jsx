8bcf2c2956d56755dc9e6481e6ed62f9ea045374
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-07-27 14:23:11 +0300

    Last Frontend Card Components

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -15,14 +15,14 @@ const Image = styled.img`
   width: 100%;
   height: ${(props) => (props.type === "sm" ? "120px" : "202px")};
   background-color: #999;
-  flex:1;
+  flex: 1;
 `;
 
 const Details = styled.div`
   display: flex;
-  margin-top: ${(props) => props.type !== "sm" && "18px"}
-  gap: 10px;
-  flex:1;
+  margin-top: ${(props) => props.type !== "sm" && "16px"};
+  gap: 12px;
+  flex: 1;
 `;
 
 const ChannelImage = styled.img`
@@ -30,7 +30,7 @@ const ChannelImage = styled.img`
   height: 36px;
   border-radius: 50%;
   background-color: #999;
-  display: ${(props)=> props.type === "sm" && "none"}
+  display: ${(props) => props.type === "sm" && "none"};
 `;
 
 const Texts = styled.div``;
