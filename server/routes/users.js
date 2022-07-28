23f5ee19aa8215079a0c78061a32a46036b71ba8
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-07-28 14:57:31 +0300

    User && Videos Crud Basic

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,29 @@
 import express from "express";
-import {  } from "../controllers/user.js";
+import {
+  deleteUser,
+  dislike,
+  getUser,
+  like,
+  subscribe,
+  unsubscribe,
+  update,
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
-
+// Update User
+router.put("/:id", verifyToken, update);
+// Delete User
+router.delete("/:id",verifyToken, deleteUser);
+// Get A User
+router.get("/find/:id", getUser);
+// Subscribe A User
+router.put("/sub/:id",verifyToken, subscribe);
+// Unsubscribe A User
+router.put("/unsub/:id",verifyToken, unsubscribe);
+// Like A Video
+router.put("/like/:videoId",verifyToken, like);
+// Dislike A Video
+router.put("/dislike/:id",verifyToken, dislike);
 export default router;
